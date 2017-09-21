2703d61fc22f8864924d57d7c557f5a7c970db96
Faateh Mohamamad <faatehim@gmail.com>
2017-09-21 11:51:02 -0400

    First commit

---
+++ public/js/custom.js
@@ -0,0 +1,89 @@
+
+!function ($) {
+    $(document).on("click","ul.nav li.parent > a ", function(){          
+        $(this).find('em').toggleClass("fa-minus");      
+    }); 
+    $(".sidebar span.icon").find('em:first').addClass("fa-plus");
+}
+
+(window.jQuery);
+	$(window).on('resize', function () {
+  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+})
+$(window).on('resize', function () {
+  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+})
+
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('em').removeClass('fa-toggle-up').addClass('fa-toggle-down');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('em').removeClass('fa-toggle-down').addClass('fa-toggle-up');
+	}
+})
+
+
+var currentSelection = "";
+function showContent(id) {
+   $(currentSelection).addClass("hidden");
+	 $(id).removeClass("hidden");
+	 currentSelection = id;
+}
+
+$(document).ready(function() {
+
+	// the basics
+	// ----------
+
+	var substringMatcher = function (strs) {
+		return function findMatches(q, cb) {
+			var matches, substringRegex;
+
+			// an array that will be populated with substring matches
+			matches = [];
+
+			// regex used to determine if a string contains the substring `q`
+			substrRegex = new RegExp(q, 'i');
+
+			// iterate through the pool of strings and for any string that
+			// contains the substring `q`, add it to the `matches` array
+			$.each(strs, function (i, str) {
+				if (substrRegex.test(str)) {
+					matches.push(str);
+				}
+			});
+
+			cb(matches);
+		};
+	};
+
+	var options = ['Blockchain','Ethereum','Bitcoin',];
+
+	$('#the-basics .typeahead').typeahead({
+			hint: true,
+			highlight: false,
+			minLength: 1.0
+		},
+		{
+			name: 'options',
+			source: substringMatcher(options)
+		});
+})
+
+// server side call
+
+function getSearchResult(e) {
+	if(event.keyCode == 13) {
+		var sq = e.value;
+		window.location.href="/resources?q=" + sq;
+
+	}
+
+
+}
+
---
+++ server.js
@@ -0,0 +1,64 @@
+require('babel-register');
+
+// var server = require('http');
+
+var logger = require('morgan');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+
+// Server side rendering
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'ejs');
+
+app.set('port', process.env.PORT || 7860);
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up our express application
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+//routes
+
+app.get("/", function(req,res) {
+    return res.render('index');
+  }
+);
+var allPages = {
+  Blockchain:"blockchain",
+  Bitcoin:"bitcoin",
+  Ethereum:"ethereum"
+
+}
+
+app.get("/resources",function(req,res) {
+  var query = req.query.q;
+
+  if (allPages[query]) {
+    return res.render(allPages[query]);
+  } else {
+    return res.render("index"); // render a page with filler info
+  }
+
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+  
+
+
+
+
